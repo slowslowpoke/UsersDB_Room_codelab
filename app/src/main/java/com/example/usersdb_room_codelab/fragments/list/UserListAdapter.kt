95e7a22eee369654779908a3ff708cc2910a970c
Amina <amina.muradalieva91@gmail.com>
2024-02-12 17:36:48 +0600

    Updatet RecyclerView to use AsyncListDiffer. Added swipe to delete item.

--- app/src/main/java/com/example/usersdb_room_codelab/fragments/list/UserListAdapter.kt
+++ app/src/main/java/com/example/usersdb_room_codelab/fragments/list/UserListAdapter.kt
@@ -3,31 +3,43 @@ package com.example.usersdb_room_codelab.fragments.list
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import com.example.usersdb_room_codelab.model.User
 import com.example.usersdb_room_codelab.databinding.UserListItemBinding
+import com.example.usersdb_room_codelab.model.User
 
 class UserListAdapter : RecyclerView.Adapter<UserListAdapter.UserViewHolder>() {
-
-    private var mUserList = emptyList<User>()
-
     class UserViewHolder(private val binding: UserListItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun bind(user: User, position: Int) {
+        fun bind(user: User) {
             binding.apply {
-                tvId.text = position.toString()
                 tvFirstName.text = user.firstName
                 tvLastName.text = user.lastName
                 tvAge.text = user.age.toString()
-                clUserListRow.setOnClickListener{
-                    val action = ListFragmentDirections.actionListFragmentToUpdateFragment(user)
-                    itemView.findNavController().navigate(action)
+                tvPosition.text = user.position
 
+                clUserListRow.setOnClickListener {
+                    val action = ListFragmentDirections.actionListFragmentToUpdateFragment(user)
+                    root.findNavController().navigate(action)
                 }
 
             }
         }
     }
+
+    private val differCallback = object : DiffUtil.ItemCallback<User>() {
+        override fun areItemsTheSame(oldItem: User, newItem: User): Boolean {
+            return (oldItem.id == newItem.id)
+        }
+
+        override fun areContentsTheSame(oldItem: User, newItem: User): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    val differ = AsyncListDiffer(this, differCallback)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
         val binding = UserListItemBinding.inflate(
@@ -39,19 +51,13 @@ class UserListAdapter : RecyclerView.Adapter<UserListAdapter.UserViewHolder>() {
         return UserViewHolder(binding)
     }
 
-    override fun getItemCount() = mUserList.size
+    override fun getItemCount() = differ.currentList.size
 
 
     override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
-        val currentUser = mUserList[position]
-        holder.bind(currentUser, position)
-
-
+        val currentUser = differ.currentList[position]
+        holder.bind(currentUser)
     }
 
-    fun setData(userList: List<User>){
-        this.mUserList = userList
-        notifyDataSetChanged()
-    }
 
 }
